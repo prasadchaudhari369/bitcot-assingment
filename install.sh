94249e8fc81ab600d9a3f36008f508f40c9cd163
Prasad Chaudhari <prasad.educational369@gmail.com>
2024-04-20 14:37:26 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,37 @@
+ #!/bin/bash
+
+# install jenkins 
+sudo apt update && sudo apt upgrade -y 
+sudo apt-get install openjdk-17-jdk -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl enable jenkins 
+
+# install docker 
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y 
+
+sudo usermod -aG docker jenkins
+sudo usermod -aG root jenkins
+sudo chmod 777 /var/run/docker.sock
+
+
+
 
